a60cb0d34cb4c30070ab06daeecb92eccfad9d01
blackneeed <121721929+blackneeed@users.noreply.github.com>
2025-08-13 21:55:05 +0200

    INITIAL: Who is qos? I don't know the guy

--- compiletime/run.sh
+++ compiletime/run.sh
@@ -11,6 +11,7 @@ fi
 mkdir -p build/boot/grub
 mkdir -p dist
 cp compiletime/grub.cfg build/boot/grub/grub.cfg
-cp $1 build/boot/qos.elf
-grub-mkrescue build -o dist/qos.iso
-qemu-system-x86_64 -cdrom dist/qos.iso -m 1G ${FLAGS}
+cp $1 build/boot/caelyx.elf
+grub-mkrescue build -o dist/caelyx.iso
+qemu-system-x86_64 -cdrom dist/caelyx.iso -m 1G ${FLAGS}
+
