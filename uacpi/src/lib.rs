bc7b46f71814a4c5ee2b5ce10acafb29998fce6e
blackneeed <121721929+blackneeed@users.noreply.github.com>
2025-11-05 19:40:46 +0100

    Fix 😝

--- uacpi/src/lib.rs
+++ uacpi/src/lib.rs
@@ -3,4 +3,12 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 
+#[unsafe(no_mangle)]
+pub extern "C" fn __ffsdi2(x: u64) -> i32 {
+    if x == 0 {
+        return 0;
+    }
+    x.trailing_zeros() as i32 + 1
+}
+
 include!(concat!(env!("OUT_DIR"), "/uacpi_bindings.rs"));
